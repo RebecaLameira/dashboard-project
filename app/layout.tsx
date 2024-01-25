2d2a30a45f4506d61252c38b735efc59a474a223
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2024-01-25 18:22:06 -0400

    Changing the css.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/img/RL.ico" sizes="any" />
+      </head>
       <body className={`${inter.className} antialiased`}>
         <LinkPortfolio />
         {children}
--- app/ui/linkportfolio.tsx
+++ app/ui/linkportfolio.tsx
@@ -5,8 +5,8 @@ import { indieFont } from './fonts';
 
 const LinkPortfolio = () => {
   return (
-    <nav className="mobile:justify-center mobile:px-8 flex h-32 w-screen items-center justify-between bg-white px-16 shadow-2xl dark:bg-black">
-      <p className=" text-wrap w-1/2 text-justify text-xl">
+    <nav className="flex h-32 w-screen items-center justify-between bg-white px-16 shadow-2xl dark:bg-black ">
+      <p className=" text-wrap w-1/2 text-justify text-sm  md:text-lg">
         <strong>Bem-vindo</strong> ao projeto{' '}
         <strong className="text-violet-400">Dashboard</strong>!! Um dos projetos
         do portfolio{' '}
@@ -22,15 +22,15 @@ const LinkPortfolio = () => {
         </a>{' '}
         disponibilizado no site na documentação do next.
       </p>
-      <Button className="h-14">
+      <Button className="h-10 md:h-14">
         <Link
           href="https://rebeca.dev.br/"
           className="flex items-center justify-between gap-4"
         >
-          <span className="text-xl font-semibold tracking-widest">
+          <span className="text-sm font-semibold tracking-widest md:text-xl">
             Portfolio
           </span>
-          <ArrowUpRightIcon className="h-8 w-8 font-semibold text-white"></ArrowUpRightIcon>
+          <ArrowUpRightIcon className="h-5 w-5 font-semibold text-white md:h-8 md:w-8"></ArrowUpRightIcon>
         </Link>
       </Button>
     </nav>
