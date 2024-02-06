92bc5b5118e880b89a8c9847cfd8740e2f3086db
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2024-02-06 19:01:19 -0400

    Chapter 7.

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,17 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Bem-vindo à Acme.</strong> Este é o exemplo do{' '}
             <a href="https://nextjs.org/learn/" className="text-violet-900">
-              Next.js Learn Course
+              Curso Next.js Learn
             </a>
-            , brought to you by Vercel.
+            , oferecido pela Vercel.
           </p>
           <Link
             href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-violet-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-violet-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Faça Login!</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
